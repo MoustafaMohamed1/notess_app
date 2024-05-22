1c5583d8d7365f8c70d719a8f2fd446a89ae8403
MoustafaMohamed1 <mostafa.034569@gmail.com>
2024-05-22 18:14:27 +0300

    create NotesViewBody ,CustomAppBAr and CustomSearchIcon

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:notessapp/views/widgets/notes_view_body.dart';
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const NotesViewBody(),
+    );
+  }
+}
---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/cupertino.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const[
+        Text('Notes',style: TextStyle(fontSize: 30),),
+      ],
+    );
+
+  }
+}
